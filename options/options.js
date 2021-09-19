195ed794b0eb08809e4fe3c9b3cddf296f8cdff2
Crowdbotics <team@crowdbotics.com>
2021-09-19 07:13:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "icevoice_pro_30673Identifier",
+  url: "https://icevoice_pro_30673Identifier.botics.co",
+  api: "https://icevoice_pro_30673Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
